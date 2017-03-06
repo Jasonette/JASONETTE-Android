43554e9b021b27c5bcd690ff5200208c5f6af5f1
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-03-06 15:19:32 -0500

    finish $geo.get

--- app/src/main/java/com/jasonette/seed/Action/JasonGeoAction.java
+++ app/src/main/java/com/jasonette/seed/Action/JasonGeoAction.java
@@ -5,6 +5,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 
 import com.jasonette.seed.Helper.JasonHelper;
@@ -37,9 +38,9 @@ public class JasonGeoAction {
                 public void onProviderDisabled(String provider) {
                 }
             };
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener, Looper.getMainLooper());
         } catch (Exception e) {
-
+            Log.d("Error", e.toString());
         }
     }
 }
