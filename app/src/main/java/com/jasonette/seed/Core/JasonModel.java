e9c5b30a6c4a3fedc5f0f791070f0eebf3cf0bbd
Maksim Lin <maks@manichord.com>
2017-07-12 16:23:12 +1000

    Only include Stetho in debug builds

--- app/src/main/java/com/jasonette/seed/Core/JasonModel.java
+++ app/src/main/java/com/jasonette/seed/Core/JasonModel.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.Log;
 
-import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.jasonette.seed.Helper.JasonHelper;
 import com.jasonette.seed.Launcher.Launcher;
 
@@ -153,9 +152,7 @@ public class JasonModel{
                     .build();
 
 
-            client = new OkHttpClient.Builder()
-                    .addNetworkInterceptor(new StethoInterceptor())
-                    .build();
+            client = ((Launcher)view.getApplication()).getHttpClient();
 
             client.newCall(request).enqueue(new Callback() {
                 @Override
--- app/src/main/java/com/jasonette/seed/Launcher/Launcher.java
+++ app/src/main/java/com/jasonette/seed/Launcher/Launcher.java
@@ -8,11 +8,8 @@ import android.content.res.Resources;
 import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
-import android.view.WindowManager;
 
 import com.bumptech.glide.request.target.ViewTarget;
-import com.facebook.stetho.Stetho;
 import com.jasonette.seed.Core.JasonViewActivity;
 import com.jasonette.seed.Helper.JasonHelper;
 import com.jasonette.seed.R;
@@ -27,7 +24,7 @@ import java.lang.reflect.Method;
 import java.util.Locale;
 import java.util.Map;
 
-import static android.R.attr.action;
+import okhttp3.OkHttpClient;
 
 public class Launcher extends Application {
     private JSONObject handlers;
@@ -68,8 +65,6 @@ public class Launcher extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        Stetho.initializeWithDefaults(this);
 
         ViewTarget.setTagId(R.id.glide_request);
 
@@ -274,4 +269,7 @@ public class Launcher extends Application {
         }
     }
 
+    public OkHttpClient getHttpClient() {
+        return new OkHttpClient.Builder().build();
+    }
 }
