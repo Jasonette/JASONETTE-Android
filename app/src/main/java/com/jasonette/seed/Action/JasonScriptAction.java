600e25ca1fe75f499fdfd7992d34963800f2da27
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-09-22 17:39:36 -0700

    $script action + Transition to st.js (#182)
    * Transition to st
    
    * $return.success fix  for when options doesn't exist
    
    * $script action
    
    * ST update
    
    * fix xhtml.js
    
    * update st
    
    * handle when there's no url based script include
    
    * data/template combo not triggering onLoad

---
+++ app/src/main/java/com/jasonette/seed/Action/JasonScriptAction.java
@@ -0,0 +1,85 @@
+package com.jasonette.seed.Action;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.jasonette.seed.Core.JasonParser;
+import com.jasonette.seed.Core.JasonRequire;
+import com.jasonette.seed.Helper.JasonHelper;
+import com.jasonette.seed.Launcher.Launcher;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by e on 9/14/17.
+ */
+
+public class JasonScriptAction {
+    public void include(final JSONObject action, final JSONObject data, final JSONObject event, final Context context) {
+        try {
+            JSONObject options = action.getJSONObject("options");
+            if(options.has("items")) {
+                JSONArray items = options.getJSONArray("items");
+                JSONObject refs = new JSONObject();
+                OkHttpClient client = ((Launcher)context.getApplicationContext()).getHttpClient();
+
+                JSONArray urlItems = new JSONArray();
+                JSONArray inlineItems = new JSONArray();
+                for (int i = 0; i < items.length() ; i++) {
+                    JSONObject item = (JSONObject) items.get(i);
+                    if (item.has("url")) {
+                        urlItems.put(item.getString("url"));
+                    } else if (item.has("text")) {
+                        inlineItems.put(item.getString("text"));
+                    }
+                }
+
+                if (urlItems.length() > 0) {
+                    CountDownLatch latch = new CountDownLatch(urlItems.length());
+                    ExecutorService taskExecutor = Executors.newFixedThreadPool(urlItems.length());
+                    for (int i = 0; i < urlItems.length() ; i++) {
+                        String url = urlItems.getString(i);
+                        taskExecutor.submit(new JasonRequire(url, latch, refs, client, context));
+                    }
+                    try {
+                        latch.await();
+                    } catch (Exception e) {
+                        Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+                    }
+                }
+
+                // remote inject
+                Iterator keys = refs.keys();
+                while (keys.hasNext()) {
+                    Object key = keys.next();
+                    String js = refs.getString((String) key);
+                    JasonParser.getInstance(context).inject(js);
+                }
+
+                // local inject (inline)
+                for (int i = 0; i < inlineItems.length() ; i++) {
+                    String js = inlineItems.getString(i);
+                    JasonParser.getInstance(context).inject(js);
+                }
+
+                JasonHelper.next("success", action, data, event, context);
+            }
+        } catch (Exception e) {
+            Log.d("Warning", e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+        }
+
+
+    }
+    public void clear(final JSONObject action, final JSONObject data, final JSONObject event, final Context context) {
+        JasonParser.getInstance(context).reset();
+        JasonHelper.next("success", action, data, event, context);
+    }
+}
--- app/src/main/java/com/jasonette/seed/Core/JasonParser.java
+++ app/src/main/java/com/jasonette/seed/Core/JasonParser.java
@@ -1,6 +1,7 @@
 package com.jasonette.seed.Core;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.eclipsesource.v8.V8;
 import com.eclipsesource.v8.V8Array;
@@ -37,15 +38,33 @@ public class JasonParser {
         {
             instance = new JasonParser();
             try {
-                String js = JasonHelper.read_file("parser", context);
+                String js = JasonHelper.read_file("st", context);
+                String xhtmljs = JasonHelper.read_file("xhtml", context);
                 instance.juice = V8.createV8Runtime();
                 instance.juice.executeVoidScript(js);
+                instance.juice.executeVoidScript(xhtmljs);
                 instance.juice.getLocker().release();
             } catch (Exception e){
                 Timber.w(e.getStackTrace()[0].getMethodName() + " : " + e.toString());
             }
         }
         return instance;
+    }
+
+    public static void inject(String js) {
+        instance.juice.executeVoidScript(js);
+    }
+    public static void reset() {
+        try {
+            String js = JasonHelper.read_file("st", context);
+            String xhtmljs = JasonHelper.read_file("xhtml", context);
+            instance.juice = V8.createV8Runtime();
+            instance.juice.executeVoidScript(js);
+            instance.juice.executeVoidScript(xhtmljs);
+            instance.juice.getLocker().release();
+        } catch (Exception e){
+            Timber.w(e.getStackTrace()[0].getMethodName() + " : " + e.toString());
+        }
     }
 
 
@@ -65,29 +84,33 @@ public class JasonParser {
                         v8Console.registerJavaMethod(console, "error", "error", new Class<?>[] { String.class });
                         v8Console.registerJavaMethod(console, "trace", "trace", new Class<?>[] {});
 
-                        V8Object parser = juice.getObject("parser");
-
                         String templateJson = template.toString();
                         String dataJson = data.toString();
                         String val = "{}";
 
+                        V8Object parser = juice.getObject("JSON");
+                        // Get global variables (excluding natively injected variables which will never be used in the template)
+                        String globals = juice.executeStringScript("JSON.stringify(Object.keys(this).filter(function(key){return ['ST', 'to_json', 'setImmediate', 'clearImmediate', 'console'].indexOf(key) === -1;}));");
                         if(data_type.equalsIgnoreCase("json")) {
-                            V8Array parameters = new V8Array(juice).push(templateJson);
+                            V8Array parameters = new V8Array(juice);
+                            parameters.push(templateJson);
                             parameters.push(dataJson);
+                            parameters.push(globals);
                             parameters.push(true);
-                            val = parser.executeStringFunction("json", parameters);
+                            val = parser.executeStringFunction("transform", parameters);
                             parameters.release();
                         } else {
                             String raw_data = data.getString("$jason");
-                            V8Array parameters = new V8Array(juice).push(templateJson);
+                            V8Array parameters = new V8Array(juice).push("html");
+                            parameters.push(templateJson);
                             parameters.push(raw_data);
+                            parameters.push(globals);
                             parameters.push(true);
-                            val = parser.executeStringFunction("html", parameters);
+                            val = juice.executeStringFunction("to_json", parameters);
                             parameters.release();
                         }
                         parser.release();
                         v8Console.release();
-
 
                         res = new JSONObject(val);
                         listener.onFinished(res);
