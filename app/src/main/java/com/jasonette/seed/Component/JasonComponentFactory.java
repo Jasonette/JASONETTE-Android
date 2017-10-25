d9dfcfb911d58b6a47dd48e94ab476c37496d062
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-10-24 22:09:44 -0700

    Switch Component
    This implements the native swtich component as discussed in https://github.com/Jasonette/Jasonette/issues/1
    
    ## Examples:
    
    - Dynamic rendering with templates: https://jasonbase.com/things/bQp0
    - Static view: https://jasonbase.com/things/yKdp

--- app/src/main/java/com/jasonette/seed/Component/JasonComponentFactory.java
+++ app/src/main/java/com/jasonette/seed/Component/JasonComponentFactory.java
@@ -35,6 +35,8 @@ public class JasonComponentFactory {
                 view = JasonMapComponent.build(prototype, component, parent, context);
             } else if(type.equalsIgnoreCase("slider")) {
                 view = JasonSliderComponent.build(prototype, component, parent, context);
+            } else if(type.equalsIgnoreCase("switch")) {
+                view = JasonSwitchComponent.build(prototype, component, parent, context);
             } else {
                 // Non-existent component warning
                 JSONObject error_component = new JSONObject(component.toString());
