b94842da79119ba532bab62077f69ed540403deb
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-12-01 15:49:45 -0800

    Support $href action with a success callback (#202)

---
+++ app/src/main/java/com/jasonette/seed/Core/JasonCallback.java
@@ -0,0 +1,25 @@
+package com.jasonette.seed.Core;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.jasonette.seed.Helper.JasonHelper;
+
+import org.json.JSONObject;
+
+public class JasonCallback {
+    public void href(Intent intent, final JSONObject options) {
+        try {
+            JSONObject action = options.getJSONObject("action");
+            JSONObject event = options.getJSONObject("event");
+            Context context = (Context)options.get("context");
+
+            String return_string = intent.getStringExtra("return");
+            JSONObject return_value = new JSONObject(return_string);
+            JasonHelper.next("success", action, return_value, event, context);
+
+        } catch (Exception e) {
+
+        }
+    }
+}
